462ec5ad0f09f2fb30c72aef3135a2048ef30976
YutaOugitani <36941433+YutaOugitani@users.noreply.github.com>
2019-11-22 21:53:57 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -275,7 +275,39 @@ controller.hears(['(.*)って呼んで'], 'direct_message,direct_mention,mention
 
 });
 
-
+controller.hears(['ラーメン'], 'direct_message,direct_mention,mention', function (bot, message) {	
+    bot.reply(message, ':ramen:いいですよね:grin:');	
+    // 会話を開始します。	
+    bot.startConversation(message, function (err, convo) {	
+        // convo.ask() で質問をします。	
+        convo.ask('私が何味が好きか当ててみてください！', [	
+            {	
+                pattern: '醤油', // マッチさせる単語	
+                callback: function (response, convo) {	
+                    // ▼ マッチした時の処理 ▼	
+                    convo.say('正解！:ok_woman:\n醤油！これぞ王道！:+1:'); // convo.say()で発言をします。	
+                    convo.next(); // convo.next()で、会話を次に進めます。通常は、会話が終了します。	
+                }	
+            },	
+            {	
+                pattern: '味噌',	
+                callback: function (response, convo) {	
+                    convo.say('正解！:ok_woman:\n寒いと味噌たべたくなります！:+1:');	
+                    convo.next();	
+                }	
+            },	
+            {	
+                default: true,	
+                callback: function (response, convo) {	
+                    // ▼ どのパターンにもマッチしない時の処理 ▼	
+                    convo.say('うーん、おしいです！:no_good:');	
+                    convo.repeat(); // convo.repeat()で、質問を繰り返します。	
+                    convo.next(); // 会話を次に進めます。この場合、最初の質問にも戻ります。	
+                }	
+            }	
+        ]);	
+    })	
+});
 
 //=========================================================
 // どれにも当てはまらなかった場合の返答
